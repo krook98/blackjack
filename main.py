1fb6334c89fd709ee583da56a7edbe6a635e5bca
sokal <sokalski.jakub98@gmail.com>
2021-11-15 19:29:05 +0100

    The game asks user for another card and checks for blackjacks

--- main.py
+++ main.py
@@ -8,19 +8,24 @@ user_score = 0
 computer_score = 0
 
 
-## choose two random cards from the deck. Cards are not removed from the deck as they are drawn.
 def deal_cards(cards, how_many):
+    """choose two random cards from the deck. Cards are not removed from the deck as they are drawn."""
     for i in range(how_many):
         card = random.choice(card_pile)
         cards.append(card)
-
     return card
 
+
 def calculate_score(cards):
+    """sumarize the score from cards"""
     score = sum(cards)
     if len(cards) == 2 and score == 21:
+        if 11 in user_cards:
+            if score > 21:
+                cards.remove(11)
+                cards.append(1)
+                score -= 10
         return 0
-
     return score
 
 
@@ -30,18 +35,25 @@ deal_cards(cards = user_cards, how_many = 2)
 deal_cards(cards = computer_cards, how_many = 2)
 calculate_score(cards = user_cards)
 calculate_score(cards = computer_cards)
+
+#show your cards and one of computers
 print(user_cards)
-print(user_score)
-print(computer_score)
 print(f"{random.choice(computer_cards)} and one unknown")
 
-if user_score > 21:
-    if 11 in user_cards:
-        user_cards.remove(11)
-        user_cards.append(1)
-        user_score -= 10
-        if user_score > 21:
-            print("You lost")
+#set the score
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+
+if user_score == 0:
+    print("You lost!")
+elif computer_score == 0:
+    print("You won!")
+else:
+    another_one = input("Do you want to draw another card? Press 'y' for yes or 'n' for no: ")
+    if another_one in ["y", "Y"]:
+        deal_cards(cards = user_cards, how_many = 1)
+    else:
+        print("Game over!")
 
 
 
